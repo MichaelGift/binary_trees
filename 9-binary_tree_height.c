efff084acd9f01513d747f37f56040d66688f316
MichaelGift <simiyugift@outlook.com>
2024-01-04 16:07:21 -0800

    Measure BT height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures length of each branch and returns the longest..
+ * @tree: Root node
+ * Return: tree height else 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return(0);
+	size_t l = 0, r = 0;
+
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((l > r) ? l : r);
+}
