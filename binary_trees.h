c6280b68d85691d65f3548235a7dc110dc263539
MichaelGift <simiyugift@outlook.com>
2024-01-04 18:03:16 -0800

    Rotate node right

--- binary_trees.h
+++ binary_trees.h
@@ -48,5 +48,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
-
-#endif
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+§#endif
