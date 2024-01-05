bfcad9961c21e1e7e55e261722c7031857e64b08
MichaelGift <simiyugift@outlook.com>
2024-01-04 16:11:36 -0800

    Fix betty issue

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,14 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures length of each branch and returns the longest..
+ * binary_tree_height - Measures length of each branch
+ * returns the longest.
  * @tree: Root node
  * Return: tree height else 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return(0);
+		return (0);
 	size_t l = 0, r = 0;
 
 	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
